f927093c1515900531974c6de6a46c19e55c3861
zencifil <savascilve@gmail.com>
2016-11-19 16:06:43 +0300

    Main and detail activities added

---
+++ app/src/main/java/com/ipsos/savascilve/ipsospt/FamListFragment.java
@@ -0,0 +1,54 @@
+package com.ipsos.savascilve.ipsospt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class FamListFragment extends Fragment {
+
+    private ArrayAdapter<String> _famListAdapter;
+
+    public FamListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        _famListAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.list_item_fam,
+                new ArrayList<String>());
+
+        ListView listView = (ListView) rootView.findViewById(R.id.famListListView);
+        listView.setAdapter(_famListAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = _famListAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+
+}
