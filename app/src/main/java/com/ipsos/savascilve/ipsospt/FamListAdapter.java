b23f059b7deb300a34fb353775492046b8233125
savas.cilve <savascilve@gmail.com>
2016-12-01 17:52:51 +0300

    Adapter and fragment updated

--- app/src/main/java/com/ipsos/savascilve/ipsospt/FamListAdapter.java
+++ app/src/main/java/com/ipsos/savascilve/ipsospt/FamListAdapter.java
@@ -3,8 +3,10 @@ package com.ipsos.savascilve.ipsospt;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by zencifil on 29/11/2016.
@@ -12,18 +14,37 @@ import android.view.ViewGroup;
 
 public class FamListAdapter extends CursorAdapter {
 
-    //TODO adapter yaz
+    public static class ViewHolder {
+        public final TextView famCodeView;
+        public final TextView famNameView;
+        public final TextView addressView;
+
+        public ViewHolder (View view) {
+            famCodeView = (TextView) view.findViewById(R.id.fam_code_list_item);
+            famNameView = (TextView) view.findViewById(R.id.fam_name_list_item);
+            addressView = (TextView) view.findViewById(R.id.fam_address_list_item);
+        }
+    }
+
     public FamListAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_fam, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
 
+        String famCode = cursor.getString(FamListFragment.COL_FAM_CODE);
+        viewHolder.famCodeView.setText(famCode);
     }
 }
--- app/src/main/java/com/ipsos/savascilve/ipsospt/FamListFragment.java
+++ app/src/main/java/com/ipsos/savascilve/ipsospt/FamListFragment.java
@@ -1,9 +1,17 @@
 package com.ipsos.savascilve.ipsospt;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,10 +22,50 @@ import com.ipsos.savascilve.ipsospt.Data.PTContract;
 import com.ipsos.savascilve.ipsospt.Model.Family;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class FamListFragment extends Fragment {
+public class FamListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private FamListAdapter _famListAdapter;
+    private ListView _listView;
+    private int _position = ListView.INVALID_POSITION;
 
-    private ArrayAdapter<String> _famListAdapter;
+    private static final String SELECTED_KEY = "selected_position";
+
+    private static final int FAM_LIST_LOADER = 0;
+    //specify the columns we need
+    private static final String[] FAM_LIST_COLUMNS = {
+            PTContract.Fam.TABLE_NAME + "." + PTContract.Fam._ID,
+            PTContract.Fam.COLUMN_FAM_CODE,
+            PTContract.Fam.COLUMN_FAM_NAME,
+            PTContract.Fam.COLUMN_CITY,
+            PTContract.Fam.COLUMN_TOWN,
+            PTContract.Fam.COLUMN_DISTRICT,
+            PTContract.Fam.COLUMN_STREET,
+            PTContract.Fam.COLUMN_ROAD,
+            PTContract.Fam.COLUMN_HOUSE_NO,
+            PTContract.Fam.COLUMN_DOOR_NO
+    };
+
+    static final int COL_ID = 0;
+    static final int COL_FAM_CODE = 1;
+    static final int COL_FAM_NAME = 2;
+    static final int COL_CITY = 3;
+    static final int COL_TOWN = 4;
+    static final int COL_DISTRICT = 5;
+    static final int COL_STREET = 6;
+    static final int COL_ROAD = 7;
+    static final int COL_HOUSE_NO = 8;
+    static final int COL_DOOR_NO = 9;
+
+    /**
+     * A callback interface that all activities containing this fragment must
+     * implement. This mechanism allows activities to be notified of item
+     * selections.
+     */
+    public interface Callback {
+        //DetailFragmentCallback for when an item has been selected.
+        public void onItemSelected(Uri dateUri);
+    }
 
     public FamListFragment() {
         // Required empty public constructor
@@ -26,32 +74,82 @@ public class FamListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Add this line in order for this fragment to handle menu events.
+        //setHasOptionsMenu(true);
     }
+
+//    @Override
+//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        inflater.inflate(R.menu.forecastfragment, menu);
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_refresh) {
+//            updateWeather();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        _famListAdapter = new FamListAdapter(getActivity(), null, 0);
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        //ArrayList<Family> demoList = new ArrayList<>();
-
-        _famListAdapter = new ArrayAdapter<>(getActivity(),
-                R.layout.list_item_fam,
-                new ArrayList<String>());
-
-        ListView listView = (ListView) rootView.findViewById(R.id.fam_list_fragment_main);
-        listView.setAdapter(_famListAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        _listView = (ListView) rootView.findViewById(R.id.fam_list_fragment_main);
+        _listView.setAdapter(_famListAdapter);
+        _listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String item = _famListAdapter.getItem(position);
-                Intent intent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, "");
-                startActivity(intent);
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                if (cursor != null) {
+
+                }
+                _position = position;
             }
         });
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(SELECTED_KEY)) {
+            _position = savedInstanceState.getInt(SELECTED_KEY);
+        }
 
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FAM_LIST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (_position != ListView.INVALID_POSITION) {
+            outState.putInt(SELECTED_KEY, _position);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
